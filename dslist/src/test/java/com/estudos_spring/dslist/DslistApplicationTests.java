7a6b597a29ecfce5ce7546e3f357f7cfa8727d43
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-08-19 08:52:57 -0300

    Criação Do Projeto

---
+++ dslist/src/test/java/com/estudos_spring/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos_spring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
